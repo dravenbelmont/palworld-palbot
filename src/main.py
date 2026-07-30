deedb7e0ea6cb154ff935af3eb19d7ba90bf0b54
dravenbelmont <dravenbelmont@gmail.com>
2026-07-29 23:37:05 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 import nextcord
 import asyncio
-import os
 from utils.database import db
 
 async def force_init_db():
